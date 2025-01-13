32eca3dd80bccdafdaba3fa35ddb7d548d5aac62
Jean-Benoit-DE-LUCA <jb.dl2706@gmail.com>
2025-01-13 16:29:39 +0100

    fix uploads/pictures folder issue + update migration

--- migrations/Version20240505092157.php
+++ migrations/Version20240505092157.php
@@ -20,7 +20,7 @@ final class Version20240505092157 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE message_contact (id INT AUTO_INCREMENT NOT NULL, user_product_id INT NOT NULL, user_send_id INT NOT NULL, message LONGTEXT NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_DCEADC34E2E3A0B6 (user_product_id), INDEX IDX_DCEADC344B9E2071 (user_send_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE message_contact (id INT AUTO_INCREMENT NOT NULL, user_product_id INT NOT NULL, user_send_id INT NOT NULL, message LONGTEXT NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', product_id INT NOT NULL, user_receive_id INT NOT NULL, INDEX IDX_DCEADC34E2E3A0B6 (user_product_id), INDEX IDX_DCEADC344B9E2071 (user_send_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE message_contact ADD CONSTRAINT FK_DCEADC34E2E3A0B6 FOREIGN KEY (user_product_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE message_contact ADD CONSTRAINT FK_DCEADC344B9E2071 FOREIGN KEY (user_send_id) REFERENCES user (id)');
     }
